f99d6c68a4634cd43103fea9e673d9c3bea4a698
oman276 <75223638+oman276@users.noreply.github.com>
2025-09-03 21:06:10 -0400

    Changed navbar to be split

--- components/layout/Navigation.tsx
+++ components/layout/Navigation.tsx
@@ -14,47 +14,42 @@ export default function Navigation() {
   const pathname = usePathname();
 
   return (
-    <nav className="flex font-header tracking-wide gap-2 -mx-2 my-10">
-      <NavButton
-        href={PATH_HOME}
-        active={pathname === PATH_HOME}
-        aria-label="Home"
-      >
-        OWENGAMES
-      </NavButton>
-      
-      <NavButton
-        href={PATH_GAMES}
-        active={pathname === PATH_GAMES}
-        aria-label="Games"
-      >
-        GAMES
-      </NavButton>
-      
-    <NavButton
-        href={PATH_WRITING}
-        active={pathname.includes(PATH_WRITING)}
-        aria-label="Writing"
-    >
-        WRITING
-      </NavButton>
-
-    <NavButton
-        href={PATH_PHOTOGRAPHY}
-        active={pathname === PATH_PHOTOGRAPHY}
-        aria-label="Photography"
-      >
-        PHOTOGRAPHY
-      </NavButton>
-
-    {/* <div className="flex-1" />
-    <NavButton
-        href={PATH_CONTACT}
-        active={pathname === PATH_CONTACT}
-        aria-label="Contact"
+    <nav className="flex font-header justify-between tracking-wide gap-2 -mx-2 my-10">
+      <div>
+        <NavButton
+          href={PATH_HOME}
+          active={pathname === PATH_HOME}
+          aria-label="Home"
         >
-        CONTACT
-        </NavButton> */}
+          OWENGAMES
+        </NavButton>
+      </div>
+      
+      <div className="flex gap-2">
+        <NavButton
+          href={PATH_GAMES}
+          active={pathname === PATH_GAMES}
+          aria-label="Games"
+        >
+          GAMES
+        </NavButton>
+      
+        <NavButton
+            href={PATH_WRITING}
+            active={pathname.includes(PATH_WRITING)}
+            aria-label="Writing"
+        >
+            WRITING
+        </NavButton>
+
+        <NavButton
+            href={PATH_PHOTOGRAPHY}
+            active={pathname === PATH_PHOTOGRAPHY}
+            aria-label="Photography"
+          >
+            PHOTOGRAPHY
+        </NavButton>
+      </div>
     </nav>
   );
 }
